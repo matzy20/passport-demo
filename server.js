d1ff250fa06a654373678f36d0965714c4ff48a6
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-28 18:51:15 -1000

    added config

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 var express = require('express');
+var CONFIG = require('./config');
+
 var app = express();
+
+var server = app.listen(CONFIG.PORT, function(){
+  console.log(CONFIG.PORT);
+  console.log('Listening on port: ', server.address().port);
+});
