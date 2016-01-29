ed2f6a7dfcb81950042c77e0988cc934258b87ff
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-28 19:10:54 -1000

    added login form

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ var CONFIG = require('./config');
 
 var app = express();
 
-var server = app.listen(CONFIG.PORT, function(){
-  console.log(CONFIG.PORT);
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/login', function (req, res){
+  res.render('login');
+});
+
+app.get('/secret', function (req, res){
+  res.render('secret');
+});
+
+app.post('/login', function (req, res){
+  //TODO implement logins
+  res.send('loggin in');
+});
+
+var server = app.listen(3000, function(){
   console.log('Listening on port: ', server.address().port);
 });
